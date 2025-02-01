640c5e51c81db03f71ce9e013ede42aa6848f753
Santosh Sutar <san.sutar@gmail.com>
2025-02-01 21:23:41 +0530

    db location change

--- src/main/dbManager.ts
+++ src/main/dbManager.ts
@@ -2,7 +2,7 @@ import Database, { type Database as DatabaseType } from 'better-sqlite3';
 
 const dbOptions = { verbose: console.log };
 
-const db: DatabaseType = new Database('./db/register.db', dbOptions);
+const db: DatabaseType = new Database('./register.db', dbOptions);
 db.pragma('journal_mode = WAL');
 
 export { db };
